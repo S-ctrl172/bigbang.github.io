3c1446c3c3364de36f5f5e0bd9fca864da49e6a4
S-ctrl172 <64020241+S-ctrl172@users.noreply.github.com>
2020-10-17 12:11:06 +0530

    my new project
    wow thats great.

---
+++ WEBSITE2/css/Area1.java
@@ -0,0 +1,58 @@
+class Area
+{ 
+    double width, height, depth; 
+  
+    // constructor used when all dimensions 
+    // specified 
+    Area(double w, double h, double d) 
+    { 
+        width = w; 
+        height = h; 
+        depth = d; 
+    } 
+  
+    // constructor used when no dimensions 
+    // specified 
+    Area() 
+    { 
+        width = height = depth = 0;
+} 
+  
+    // constructor used when cube is created 
+    Area(double len) 
+    { 
+        width = height = depth = len; 
+    } 
+  
+    // compute and return volume 
+    double volume() 
+    { 
+        return width * height * depth; 
+    } 
+} 
+  
+// Driver code 
+public class Test 
+{ 
+    public static void main(String args[]) 
+    { 
+        // create boxes using the various 
+        // constructors 
+        Area myArea1 = new Area(10, 20, 15); 
+        Area myArea2 = new Area(); 
+        Area mycube = new Area(7); 
+  
+        double vol; 
+// get volume of first box 
+        vol = mybox1.volume(); 
+        System.out.println(" Volume of mybox1 is " + vol); 
+  
+        // get volume of second box 
+        vol = mybox2.volume(); 
+        System.out.println(" Volume of mybox2 is " + vol); 
+  
+        // get volume of cube 
+        vol = mycube.volume(); 
+        System.out.println(" Volume of mycube is " + vol); 
+    } 
+} 
